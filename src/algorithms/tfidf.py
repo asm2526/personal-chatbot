4f3d4f0443f76d2a11185d986662531941888fe2
asm2526 <asm25@email.sc.edu>
2025-09-11 01:15:30 -0400

    prepping tfidf similarity algorithm for large intelligence jump. Will allow TF-IDF to have multi-intent matching. next implementation will be adding sentence-transformers

--- src/algorithms/tfidf.py
+++ src/algorithms/tfidf.py
@@ -17,7 +17,10 @@ class TfidfMatcher:
         self.vectorizer = TfidfVectorizer()
         # Build corpus from intent names + responses
         self.intent_names = [i["intent"] for i in intents]
-        self.corpus = [i["intent"] + " " + " ".join(i.get("responses", [])) for i in intents]
+        self.corpus = [
+            i["intents"] + " " + " ".join(i.get("responses", []))
+            for i in intents
+        ]
         self.tfidf_matrix = self.vectorizer.fit_transform(self.corpus)
     
     def match(self, message, threshold=0.1):
@@ -31,9 +34,9 @@ class TfidfMatcher:
         query_vec = self.vectorizer.transform([message])
         similarities = cosine_similarity(query_vec, self.tfidf_matrix).flatten()
 
-        best_idx = np.argmax(similarities)
-        best_score = similarities[best_idx]
-
-        if best_score >= threshold:
-            return self.intents[best_idx]
-        return None
+        matches = []
+        for idx, score in enumerate(similarities):
+            if score >= threshold:
+                matches.append(self.intents[idx])
+        
+        return matches # could be empty, 1, or many
